e9c7c465dfae002ba7ed8ecc79d1143d332a41d0
Aleja <Aleja@10.72.132.33>
2017-07-25 22:55:05 -0500

    terminado, sin dificultad alguna

---
+++ public_html/p16/js/16.js
@@ -0,0 +1,11 @@
+
+var base = parseInt(prompt('Type the base'));
+var height = parseInt(prompt('Type the height'));
+
+perimeter = base + base + height + height;
+
+   console.log("Perimeter is  = " + perimeter);
+   
+area = base * height;
+
+   console.log("Area is = " + area);
