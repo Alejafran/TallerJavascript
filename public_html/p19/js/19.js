c90ee1dceceb915c59f66a219b086dc55aa5c287
Aleja <Aleja@10.72.132.33>
2017-07-25 23:41:52 -0500

    terminado sin problemas.

---
+++ public_html/p19/js/19.js
@@ -0,0 +1,12 @@
+
+var number = parseInt(prompt('Type a whole number'));
+
+if (number<0){
+   
+   operation= number * (-1);
+   console.log(operation);
+}
+else{
+   
+   console.log("Should Enter a number less than zero");
+}
