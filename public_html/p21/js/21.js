e8b0c2b84722aba74b0f9856c58c2078aea53293
Aleja <Aleja@10.72.132.33>
2017-07-28 01:28:44 -0500

    logica terminada, no sé por que no spe ve en consola

---
+++ public_html/p21/js/21.js
@@ -0,0 +1,18 @@
+
+function calculate() {
+   
+   var One = parseInt(prompt("Type a value for a " ));
+   var Two = parseInt(prompt("Type a value for b " ));
+   
+   var a = Math.pow(One, 2);
+   var b = Math.pow(Two, 2);
+   var c= a + b;
+   var d = Math.pow(c, 0,5);
+   
+   console.log="hypotenuse= X²= a² + b² ";
+   console.log="hypotenuse= X²= "+ One +"² " + Two +"² = " + b;
+   
+   }
+   
+   
+   calculate ();
