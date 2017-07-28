71f8ec26d726d1945cfe5646ab4bda2709b284d0
Aleja <Aleja@10.72.132.33>
2017-07-28 17:52:11 -0500

    terminado

--- public_html/p20/js/20.js
+++ public_html/p20/js/20.js
@@ -1,24 +1,30 @@
 
 var number = parseInt(prompt('Type your salary'));
+var days = parseInt(prompt('Type a number of days workerd'));
+
+var x= number/30;
+var total= x * days;
 
 if(number<="1475434" || number=="1'475.434") {
    
-   totalSalary= number + 83140; 
+   totalSalary= total + 83140; 
    
    console.log("The aid is granted");
    console.log("Salary: "+ number);
+   console.log("Days worked: "+ days);
    console.log("Transportation aid: 83.140 ");
    console.log("Total Salary: "+ totalSalary);
  }    
  
  if(number>= "2950868" || number>= "2'950.868"){
     
-    totalSalary= (number * 1) / 100;
     console.log("Applies Retention at source");
-    console.log("Salary: " + number);
+    totalSalary= (number * 1) / 100;
     console.log("Retention: " + totalSalary);
+    console.log("Salary: " + number);
+    console.log("Days worked: "+ days);
     
-    x=number-totalSalary;
-    console.log(x);
+    x= total - totalSalary; 
+    console.log("Total Salary: "+x);
  };
  
