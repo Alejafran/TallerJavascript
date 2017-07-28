5a19e84f7de451664d28552f839f5bf3c76c917f
Aleja <Aleja@10.72.132.33>
2017-07-28 16:50:01 -0500

    terminado

---
+++ public_html/p05/js/05.js
@@ -0,0 +1,7 @@
+
+var name = prompt("Type your complete name");
+var id = prompt("what is your id type?");
+var number = prompt("Type your id number");
+
+console.log("Hello my name is " + name );
+console.log("My number of " + id + " is " + number);
