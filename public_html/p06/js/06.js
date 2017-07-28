d290ace213df27067a34840c20be1ac03fa79d3d
Aleja <Aleja@10.72.132.33>
2017-07-28 16:49:52 -0500

    terminado

---
+++ public_html/p06/js/06.js
@@ -0,0 +1,30 @@
+
+function calculate(){
+   
+   var result = Math.pow(50 , 10);
+   console.log("50 ^ 10 = " + result);
+   
+   var result = Math.pow(10, 50);
+   console.log("10 ^ 50 = " + result);
+   
+   var result = 50 * 10 ;
+   console.log("50 * 10 = " + result);
+   
+   var result = 50 / 10 ;
+   console.log("50 / 10 = " + result);
+   
+   var result = 10 / 50 ;
+   console.log("10 / 50 = " + result);
+   
+   var result = 50 % 10 ;
+   console.log("The division module between 50 and 10 is = " + result);
+   
+   var result = 50 - 10 ;
+   console.log("50 - 10 = " + result);
+   
+   var result = 50 + 10 ;
+   console.log("50 + 10 = " + result);
+
+}
+
+calculate ();
