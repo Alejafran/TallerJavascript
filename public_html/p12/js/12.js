0457ce9d6032c26ccc119bf1b302848ac5fb8e74
Aleja <Aleja@10.72.132.33>
2017-07-28 16:48:34 -0500

    no comprendo como funciona el ciclo Do while

---
+++ public_html/p12/js/12.js
@@ -0,0 +1,30 @@
+
+var e=6;
+var a=1;
+var s="*";
+
+while (e>=1){
+   
+   var i=1;
+   var d=1;
+   
+   while(i<=e){
+      
+      console.log(" ");
+      
+      i++;
+   }
+   
+   while(d<=a){
+      
+      console.log(s);
+      
+      d++;
+   }
+   
+   e--;
+   
+   a++;
+   
+   console.log("\u0009");
+}
