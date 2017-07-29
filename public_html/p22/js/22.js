d1dc6e939c24d6bafd4ef3af309cb435f386f971
Aleja <Aleja@10.72.132.33>
2017-07-28 21:32:48 -0500

    terminado

---
+++ public_html/p22/js/22.js
@@ -0,0 +1,17 @@
+
+console.log("In 15 Year there are:");
+
+var number = 12 * 15;
+console.log("months : "+number);
+
+var number = 48 * 15;
+console.log("Weeks: "+number);
+
+var number = 8760 * 15;
+console.log("Hours : "+number);
+
+var number = 525600 * 15 ;
+console.log("Minutes : "+number);
+
+var number = 31536000 * 15;
+console.log("Seconds : "+number);
