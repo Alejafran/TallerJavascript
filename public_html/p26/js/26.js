5b1767372b0f2d9bd983d11d334e13cb0c64d5ef
Aleja <Aleja@10.72.132.33>
2017-08-05 02:54:07 -0500

    terminado, con dudas, se me dificultó vastante en manejo de la estructura switch, pero al final ya está comprendido

---
+++ public_html/p26/js/26.js
@@ -0,0 +1,88 @@
+
+function option () {
+   
+   var menu = prompt("\
+   1.suma \n\
+   2. resta \n\
+   3. multiplicacion \n\
+   4. división \n\
+   0. salir ");
+   
+   return parseInt(menu);
+};
+
+var result = option();
+
+ switch (result) {
+  case 0:
+    console.log("Goodbye");
+    break;
+  case 1:
+    addition();
+    break;
+
+  case 2:
+    substraction();
+    break;
+    
+ case 3:
+    multiplication();
+    break;
+    
+ case 4:
+    division();
+    break;
+    
+  default:
+    console.log("Type a number from 1 to 4");
+}; 
+
+
+
+function addition (){
+
+   var one = parseInt(prompt("Type a number"));
+   var two = parseInt(prompt("Type other number"));
+   
+   answer= one + two;
+   
+   console.log(one + " + " +two+ " = "+ answer);
+
+   return answer;
+};
+
+function substraction(){
+   
+   var one = parseInt(prompt("Type a number"));
+   var two = parseInt(prompt("Type other number"));
+   
+   answer= one - two;
+   
+   console.log(one + " - " +two+ " = "+ answer);
+
+   return answer;
+};
+ 
+ function multiplication(){
+   
+   var one = parseInt(prompt("Type a number"));
+   var two = parseInt(prompt("Type other number"));
+   
+   answer= one * two;
+   
+   console.log(one + " * " +two+ " = "+ answer);
+
+   return answer;
+};
+
+ function division (){
+   
+   var one = parseInt(prompt("Type a number"));
+   var two = parseInt(prompt("Type other number"));
+   
+   answer= one / two;
+   
+   console.log(one + " / " +two+ " = "+ answer);
+
+   return answer;
+};
