1bb5cec52f14d988f4bc542eefd93312e38c89a3
Aleja <Aleja@10.72.132.33>
2017-08-16 01:23:04 -0500

    terminado

---
+++ public_html/p24/js/24.js
@@ -0,0 +1,11 @@
+ 
+var hour = parseInt(prompt("Type a number of the hours"));
+
+var week = hour /(24*7);
+
+var day = hour/24;
+
+console.log("Days and Weekends of " + hour + " hours");
+console.log("Weeks: "+week);
+console.log("Days: "+day);
+
