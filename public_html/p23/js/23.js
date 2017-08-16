55b99c833f7ad7f8d3bc5876939ae5514e4f8f89
Aleja <Aleja@10.72.132.33>
2017-08-16 02:14:03 -0500

    terminado

--- public_html/p23/js/23.js
+++ public_html/p23/js/23.js
@@ -1,8 +1,8 @@
 
 function years() {
    
-   var date1 = prompt("Type a date  YY,MM,DD");
-   var date2 = prompt("Type other date YY,MM,DD");
+   var date1 = prompt("Type a bigger date  YY,MM,DD");
+   var date2 = prompt("Type a minor date YY,MM,DD");
 
    var one = new Date(date1);
    var two = new Date(date2);
@@ -16,9 +16,9 @@ function years() {
    console.log(date1 + " - " + date2);
    console.log();
 
-   console.log("there are " + day + " Days between the year " + date1 + " and " + date2);
-   console.log("there are " + month + " Months between the year " + date1 + " and " + date2);
-   console.log("there are " + year + " years between the year " + date1 + " and " + date2);
+   console.log("there are " + day + " Days between the dates " + date1 + " and " + date2);
+   console.log("there are " + month + " Months between the dates " + date1 + " and " + date2);
+   console.log("there are " + year + " years between the dates " + date1 + " and " + date2);
 }
 
 years ();
