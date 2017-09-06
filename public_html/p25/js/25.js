5267a0ce01df0be2c7ddfad4abc4b33b5c4e54d0
Aleja <Aleja@10.72.132.33>
2017-09-06 16:06:14 -0500

    corrigiendo

--- public_html/p25/js/25.js
+++ public_html/p25/js/25.js
@@ -1,40 +1,62 @@
 
-
 var number = new Array(10);
-var sp = 0; //acumulador de pares
-var cp = 0; //cantidad de pares
-var NumberP=true;
+var i=0;
+var sp=0;
+var cp =0;
 
-   for (i=0;i<=9;i++){
-      
-      number[i]= parseInt(prompt('Type a number'));
+while(i !== 10){
+
+   number[i] = parseInt(prompt("Type any number"));
+
+   i++;
+}
+//numeros pares ----------------------------------------------------------------
+
+console.log("Pair Numbers");
+
+for (i=0; i<=number.length; i++){
+
+   if(number[i]%2 == 0){
+
+      console.log(number[i]);
+      sp = sp+number[i];
+      cp++;
    }
-      
-   console.log("Numeros pares ");
-   
-   for (i=0; i<=number.length; i++){
-      if(number[i]%2 == 0){
-         console.log(number[i]);
-         sp = sp+number[i];
-         cp++;
-      };
-   };
-   
-   console.log("Sumatoria de pares: " + sp );
-   console.log("promedio de pares: " + (sp/cp));
-   
-   console.log("Numeros Primos:");
-   
-      for(i=0;i<=number.length; i++){
+}
+  
+console.log("Sumatoria de pares: " + sp );
+console.log("promedio de pares: " + (sp/cp));
 
-         if(number[i]/i == Math.round(number[i]/i) && i!=1 && i!=number[i]){
-            
-            console.log(number[i]);
-            sp = sp+number[i];
-            cp++;
-         };
-      };
-   console.log("Sumatoria de primos: " + sp );
-   console.log("promedio de primos: " + (sp/cp));
-      
-      
+//numeros primos ---------------------------------------------------------------
+
+for (i=0; i<=number.length; i++){
+
+   var a=0;
+   var flag=0;
+   var s=0;
+   var c = 0;
+
+   while(flag<=number[i]){
+
+      if (number[i]%i==0){
+
+          a++;
+
+        }
+
+       flag++;
+   }
+
+   if(a==2){
+
+      s = s+number[i];
+      c++;
+
+      console.log(number[i]);
+      console.log("Sumatoria de pares: " + s );
+      console.log("promedio de pares: " + (s/c));
+   }
+
+   i++;
+}
+
