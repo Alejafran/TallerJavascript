08cb279205ebb8ebf941152ec14e55ac8bb61199
Aleja <Aleja@10.72.132.33>
2017-09-08 17:36:28 -0500

    terminado

--- public_html/p29/js/29.js
+++ public_html/p29/js/29.js
@@ -1,3 +1,5 @@
+var items = new Array();
+
 function option1() {
 
    menu = prompt("\
@@ -10,74 +12,120 @@ function option1() {
 
    return parseInt(menu);
 }
-;
 
+//---------------------------------------------------------------------------------------------------------------------
 function save() {
+   do {
+      var rsp = parseInt(prompt("Type '1' to continue\n\ Type '2' to return"));
+      if (rsp === 1) {
+         var items = JSON.parse(localStorage.getItem("items"));
+         if (items === null) {
+            items = [];
+         }
+         var name = prompt("Type a name of item");
+         var code = prompt("type a number of code");
 
-   items = new Array();
-
-   for (var i = 0; i === items.length; i++) {
-
-      var number = parseInt(prompt("Type '4' to back \n\Type '5' to continue add items"));
-      if (number === 4) {
-
+         items.push({name, code});
+         localStorage.setItem("items", JSON.stringify(items));
+      }
+      if (rsp === 2) {
          break
       }
-      if (number === 5) {
-
-         items.push({
-            name: prompt("Type a name of item"),
-            code: prompt("type a number of code")
-         });
-
-      }
-
-      localStorage.setItem("items", JSON.stringify(items));
-   }
-
-   return items;
+   } while (rsp !== 2)
 }
+//------------------------------------------------------------------------------------------------------------
 
 function searchName() {
 
-   var text = prompt("type a name of item that you wants search");
-
    var one = JSON.parse(localStorage.getItem("items"));
 
-   for (i = 0; i <= items.length; i++) {
+   if (one === null) {
 
-      if (text === one[i].name) {
+      alert('the database is empty');
 
-         console.log("Name Item: " + one[i].name + ", " + " Code Item:  " + one[i].code);
+   } else {
+
+      function sn() {
+         var text = prompt("type a name of item that you wants search");
+         var flag = 0;
+
+         for (i = 0; i <= one.length - 1; i++) {
+
+            if (text === one[i].name) {
+
+               console.log("Name Item: " + one[i].name + ", " + " Code Item:  " + one[i].code);
+               flag++;
+            }
+         }
+         if (flag === 0) {
+            console.log("Item no found");
+         }
+         var answer = parseInt(prompt("Type '1' to continue\n\ Type 2 to return"));
+         return answer;
       }
+      var answer1;
+
+      do {
+         answer = sn();
+
+         if (answer === 1) {
+            answer1 = sn();
+         }
+         if (answer === 2) {
+            break
+         }
+      } while (answer !== 2);
    }
-
-   return one;
-
 }
+//-------------------------------------------------------------------------------------------------------------
 
 function searchCode() {
 
-   var text = prompt("type a Code of item that you wants search");
-
    var one = JSON.parse(localStorage.getItem("items"));
 
-   for (i = 0; i <= items.length; i++) {
+   if (one === null) {
 
-      if (text === one[i].code) {
+      alert('the database is empty');
 
-         console.log("Name Item: " + one[i].name + ", " + " Code Item:  " + one[i].code);
+   } else {
+
+      function sc () {
+         var text = prompt("type a Code of item that you wants search");
+         var flag = 0;
+
+         for (i = 0; i <= one.length - 1; i++) {
+            
+            if (text === one[i].code) {
+
+               console.log("Name Item: " + one[i].name + ", " + " Code Item:  " + one[i].code);
+               flag++;
+            }
+         }
+         if (flag === 0) {
+            console.log("Item no found");
+         }
+         var answer = parseInt(prompt("Type '1' to continue\n\ Type 2 to return"));
+         return answer;
       }
+      var answer1;
+
+      do {
+         answer = sc();
+
+         if (answer === 1) {
+            answer1 = sc();
+         }
+         if (answer === 2) {
+            break
+         }
+      } while (answer !== 2);
    }
-
-   return one;
-
 }
+//menu--------------------------------------------------------------------------------------------------------
 
+var menu, i, option, name;
 
-var menu, i = 0, option, name;
-
-while (option !== 0) {
+do {
 
    option = option1();
 
@@ -99,6 +147,5 @@ while (option !== 0) {
 
       default:
          console.log("Type a number from 0 to 3");
-         break;
    }
-}
+} while (option !== 0);
